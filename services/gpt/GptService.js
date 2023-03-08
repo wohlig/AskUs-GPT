7c0ca03883ca2bfacb8bceef92500c5928f2f617
BilalWohlig <wohligbilal@gmail.com>
2023-03-08 11:23:30 +0530

    changes

--- services/gpt/GptService.js
+++ services/gpt/GptService.js
@@ -3,16 +3,21 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(configuration)
+
 class GptService {
   async getAnsFromGPT (context, question) {
     console.log('Sending Question to GPT')
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `This is a news article: 
-        ${context}
-        AskUs is a chatbot that answers any question within the scope of the above news article. If the question is outside the scope of the news article, AskUs will respond with "Sorry, I don't know". If the user acknowledges the answer or writes any form of 'okay' slang, AskUs will respond with 👍. Do not generate questions and answers on your own.
-        Question: ${question}
-        Answer: `,
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: `Your name is AskUs and you are a helpful chatbot. AskUs answers any question within the scope of the below news article. If the question is outside the scope of the news article, AskUs will respond with "Sorry, I don't know". If the user acknowledges the answer or writes any form of 'okay' slang, AskUs will respond with 👍. Do not generate questions and answers on your own. This is the context of the article: ${context}` },
+        {
+          role: 'user', content: `Question: ${question}`
+        },
+        {
+          role: 'assistant', content: 'Answer: '
+        }
+      ],
       temperature: 0,
       max_tokens: 256,
       top_p: 1,
@@ -20,25 +25,47 @@ class GptService {
       presence_penalty: 0
     })
     return response.data
+    // const response = await openai.createCompletion({
+    //   model: 'text-davinci-003',
+    //   prompt: `This is a news article:
+    //     ${context}
+    //     AskUs is a chatbot that answers any question within the scope of the above news article. If the question is outside the scope of the news article, AskUs will respond with "Sorry, I don't know". If the user acknowledges the answer or writes any form of 'okay' slang, AskUs will respond with 👍. Do not generate questions and answers on your own.
+    //     Question: ${question}
+    //     Answer: `,
+    //   temperature: 0,
+    //   max_tokens: 256,
+    //   top_p: 1,
+    //   frequency_penalty: 0,
+    //   presence_penalty: 0
+    // })
+    // return response.data
   }
 
   async getContentFromGPT (context) {
     console.log('Sending News to GPT')
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `${context}
-        1. Create a summary of the above article in the range of 60-80 words.
-        2. Create a headline for the summary.
-        3. Create a tweet for the news article.
-        4. Create tags for the above article.
-        First give the summary, then the headline, then the tweet and finally the tags.`,
-      temperature: 0,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0
-    })
-    return response.data
+    try {
+      const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant. First give the summary, label it as "Summary:", then the headline, label it as "Headline:" then the tweet, label it as "Tweet:" and finally the tags, label it as "Tags:". ' },
+          {
+            role: 'user', content: `${context}
+          1. Create a summary of the above article in the range of 60-80 words.
+          2. Create a headline for the summary.
+          3. Create a tweet for the news article.
+          4. Create tags for the above article.`
+          }
+        ],
+        temperature: 0,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0
+      })
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
