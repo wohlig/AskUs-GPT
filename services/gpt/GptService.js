ed1eff24b2806a15356a809d5d064c9ae4463526
BilalWohlig <wohligbilal@gmail.com>
2023-03-28 16:03:28 +0530

    changes

--- services/gpt/GptService.js
+++ services/gpt/GptService.js
@@ -45,7 +45,7 @@ class GptService {
           }
         ],
         temperature: 0,
-        max_tokens: 500,
+        max_tokens: 2048,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0
