4d03bd447e81446af4c35c608d296acc6979301e
BilalWohlig <wohligbilal@gmail.com>
2023-04-04 12:18:24 +0530

    changes

--- services/gnews/GnewsService.js
+++ services/gnews/GnewsService.js
@@ -3,7 +3,7 @@ class GnewsService {
   async getNewsFromGNews (categoryName, from, to, lang) {
     console.log('Fetching News From API')
     const news = await axios.get(
-        `https://gnews.io/api/v4/top-headlines?category=${categoryName}&token=${process.env.TOKEN}&expand=content&lang=${lang}&country=in&to=${to}&from=${from}`
+        `https://gnews.io/api/v4/top-headlines?category=${categoryName}&token=${process.env.TOKEN}&expand=content&lang=${lang}&country=in&to=${to}&from=${from}&max=50`
     )
     return news.data
   }
