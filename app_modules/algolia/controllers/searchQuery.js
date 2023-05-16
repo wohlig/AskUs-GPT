2c4663fb494a18f81ba87905e29b69f71d67acf5
samayyy <samaykgada09@gmail.com>
2023-05-16 17:31:29 +0530

    changes

--- app_modules/algolia/controllers/searchQuery.js
+++ app_modules/algolia/controllers/searchQuery.js
@@ -46,7 +46,7 @@ const validation = (req, res, next) => {
 }
 const algoliaSearchQuery = async (req, res) => {
   try {
-    const result = await AlgoliaService.searchQueryAlgolia(req.body.searchData, req.body.page, req.body.language, req.body.userId, req.body.blockedSources)
+    const result = await AlgoliaService.searchQueryAlgolia(req.body.searchData, req.body.page, req.body.language, req.body.userId, req.body.blockedSources, req.body.categories)
     res.sendJson({ type: __constants.RESPONSE_MESSAGES.SUCCESS, data: result })
   } catch (err) {
     console.log('algoliaSearchQuery Error', err)
