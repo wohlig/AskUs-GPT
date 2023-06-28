4f96e0821ffd039d31fbdc4c9913734c7a12d10a
BilalWohlig <wohligbilal@gmail.com>
2023-06-28 15:32:00 +0530

    pretty

--- services/gnews/GnewsService.js
+++ services/gnews/GnewsService.js
@@ -1,26 +1,26 @@
-const axios = require("axios");
+const axios = require('axios')
 class GnewsService {
-  async getNewsFromGNews(categoryName, from, to, lang) {
+  async getNewsFromGNews (categoryName, from, to, lang) {
     console.log({
       categoryName: categoryName,
       from: from,
       to: to,
-      lang: lang,
-    });
-    console.log("Fetching News From API");
+      lang: lang
+    })
+    console.log('Fetching News From API')
     try {
       const news = await axios.get(
         `https://gnews.io/api/v4/top-headlines?category=${categoryName}&token=${process.env.TOKEN}&expand=content&lang=${lang}&country=in&to=${to}&from=${from}&max=50`
-      );
-      console.log("New News", news.data);
-      return news.data;
+      )
+      console.log('New News', news.data)
+      return news.data
     } catch (error) {
       await axios.post(
         `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=@newsShieldLogs&text=Title:%20GNEWS%20Error\n\nMessage:%20${error.message}`
-    )
+      )
       throw new Error(error.message)
     }
   }
 }
 
-module.exports = new GnewsService();
+module.exports = new GnewsService()
