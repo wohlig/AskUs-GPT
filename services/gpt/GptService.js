c7750457a4543b75a0f559c4eb5dd51ae737aef9
BilalWohlig <wohligbilal@gmail.com>
2023-10-17 11:22:36 +0530

    changes`

--- services/gpt/GptService.js
+++ services/gpt/GptService.js
@@ -51,7 +51,7 @@ class GptService {
           {
             role: 'user',
             content: `${context}
-          1. Create a summary of the above article in the range of 60 words. It is very important for the summary to be exactly 60 words. Do not go over or under this length.
+          1. Provide a summary of the key points from the article above. The summary should be exactly 60 words in length. Focus on capturing the main ideas and key details in a clear and concise way. Ensure the full summary is 60 words, do not go over or under. Summarize the essence of the article accurately regardless of its length.
           2. Create a headline for the summary.
           3. Create a tweet for the news article.
           4. Create tags for the above article.
