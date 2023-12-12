3fa0847670fc9c66ce9496d68cb878ce1d5cf237
BilalWohlig <wohligbilal@gmail.com>
2023-12-12 16:05:31 +0530

    changes

--- services/gpt/GptService.js
+++ services/gpt/GptService.js
@@ -33,7 +33,7 @@ class GptService {
     return response.data
   }
 
-  async getContentFromGPT (context, language, type, max_tokens = 1000, model = 'gpt-3.5-turbo') {
+  async getContentFromGPT (context, language, type, trends, max_tokens = 1000, model = 'gpt-3.5-turbo-16k') {
     if (type == 'YouTube') {
       max_tokens = 5000,
       model = 'gpt-3.5-turbo-16k'
@@ -46,7 +46,7 @@ class GptService {
           {
             role: 'system',
             content:
-              'You are a helpful assistant. First give the summary, label it as "Summary:", then the headline, label it as "Headline:" then the tweet, label it as "Tweet:", then the tags, label it as "Tags:", and finally the bullet points, label it as "Bullets:".'
+              'You are a helpful assistant. First give the summary, label it as "Summary:", then the headline, label it as "Headline:" then the tweet, label it as "Tweet:", then the tags, label it as "Tags:", then the bullet points, label it as "Bullets:", and finally the similarity scores, label them as "Similarities:".'
           },
           {
             role: 'user',
@@ -55,7 +55,9 @@ class GptService {
           2. Create a headline in under 20 words for the summary.
           3. Create a tweet for the news article.
           4. Create tags for the above article.
-          5. Give the same summary created above in bullet points.`
+          5. Give the same summary created above in bullet points.
+          6. Compare the news article provided above with each array from the trending tags provided below and then give a similarity score (no decimal scores) out of 10 for every array from the trending tags. Provide only the similarity scores in a single line removing any preceding serial numbers or letters.
+          ${trends}`
           }
         ]
       } else {
@@ -70,7 +72,9 @@ class GptService {
             content: `${context}
           1. Create a summary of the above article strictly in ${language} language in the range of 60 words. It is very important for the summary to be exactly 60 words. Do not go over or under this length.
           2. Create a headline in under 20 words for the summary strictly in ${language} language.
-          3. Create tags for the above article strictly in ${language} language.`
+          3. Create tags for the above article strictly in ${language} language.
+          4. Compare the news article provided above with each array from the trending tags provided below and then give a similarity score out of 10 for every array from the trending tags. Provide only the similarity scores in a single line removing any preceding serial numbers or letters.
+          ${trends}`
           }
         ]
       }
