ab08552bc4e4efc3b7d3292dbbc442629e96668e
BilalWohlig <wohligbilal@gmail.com>
2024-03-22 11:37:15 +0530

    changes

--- services/translate/TranslateService.js
+++ services/translate/TranslateService.js
@@ -2,45 +2,84 @@ const cache = require("../../middlewares/requestCacheMiddleware");
 const { TranslationServiceClient } = require("@google-cloud/translate");
 if (process.env.GOOGLE_TRANSLATE_CREDENTIALS) {
   try {
-      var translateKey = JSON.parse(
-          process.env.GOOGLE_TRANSLATE_CREDENTIALS
-      )
+    var translateKey = JSON.parse(process.env.GOOGLE_TRANSLATE_CREDENTIALS);
   } catch (error) {
-      console.error("Error reading the JSON file:", error)
+    console.error("Error reading the JSON file:", error);
   }
 }
 class TranslateService {
   async getTranslation(text, lang) {
     try {
-      const dataFromRedis = await cache.get(text+lang)
-      if(!dataFromRedis) {
-          const translationClient = new TranslationServiceClient(
-            translateKey
-                    ? {
-                          credentials: translateKey,
-                          projectId: translateKey.project_id
-                      }
-                    : {}
-          );
-          const request = {
-              parent: `projects/${process.env.PROJECT_ID}/locations/${process.env.PROJECT_LOCATION}`,
-              contents: [text],
-              mimeType: 'text/plain', // mime types: text/plain, text/html
-              sourceLanguageCode: 'en',
-              targetLanguageCode: lang
-            };
+      const dataFromRedis = await cache.get(text + lang);
+      if (!dataFromRedis) {
+        const translationClient = new TranslationServiceClient(
+          translateKey
+            ? {
+                credentials: translateKey,
+                projectId: translateKey.project_id,
+              }
+            : {}
+        );
+        const request = {
+          parent: `projects/${process.env.PROJECT_ID}/locations/${process.env.PROJECT_LOCATION}`,
+          contents: [text],
+          mimeType: "text/plain", // mime types: text/plain, text/html
+          sourceLanguageCode: "en",
+          targetLanguageCode: lang,
+        };
 
-            // Run request
-            const [response] = await translationClient.translateText(request);
-            await cache.setWithoutEx(text+lang, response.translations[0].translatedText)
-            return response.translations[0].translatedText
+        // Run request
+        const [response] = await translationClient.translateText(request);
+        await cache.setWithoutEx(
+          text + lang,
+          response.translations[0].translatedText
+        );
+        return response.translations[0].translatedText;
       }
-      return dataFromRedis
+      return dataFromRedis;
     } catch (error) {
       console.error(error);
     }
   }
 
+  convertToHindiNumbers(inputString) {
+    const hindiDigits = {
+      0: "०",
+      1: "१",
+      2: "२",
+      3: "३",
+      4: "४",
+      5: "५",
+      6: "६",
+      7: "७",
+      8: "८",
+      9: "९",
+    };
+
+    return this.convertNumbers(inputString, hindiDigits);
+  }
+  convertToGujaratiNumbers(inputString) {
+    const gujaratiDigits = {
+      0: "૦",
+      1: "૧",
+      2: "૨",
+      3: "૩",
+      4: "૪",
+      5: "૫",
+      6: "૬",
+      7: "૭",
+      8: "૮",
+      9: "૯",
+    };
+
+    return this.convertNumbers(inputString, gujaratiDigits);
+  }
+
+  convertNumbers(inputString, digitMap) {
+    return inputString.replace(/\d/g, function (match) {
+        return digitMap[match]
+    })
+}
   async predictionTranslation(predictionObj, lang) {
     const birth_moon_sign = await this.getTranslation(
       predictionObj.birth_moon_sign,
@@ -73,12 +112,13 @@ class TranslateService {
       predictionObj.prediction.travel,
       lang
     );
-    const healthText = await this.getTranslation('Health', lang)
-    const emotionsText = await this.getTranslation('Emotions', lang)
-    const professionText = await this.getTranslation('Profession', lang)
-    const luckText = await this.getTranslation('Luck', lang)
-    const personal_lifeText = await this.getTranslation('Personal Life', lang)
-    const travelText = await this.getTranslation('Travel', lang)
+    const prediction_date = await this.getTranslation(predictionObj.prediction_date, lang);
+    const healthText = await this.getTranslation("Health", lang);
+    const emotionsText = await this.getTranslation("Emotions", lang);
+    const professionText = await this.getTranslation("Profession", lang);
+    const luckText = await this.getTranslation("Luck", lang);
+    const personal_lifeText = await this.getTranslation("Personal Life", lang);
+    const travelText = await this.getTranslation("Travel", lang);
     const finalTranslatedPredicitonObj = {
       health: health,
       emotions: emotions,
@@ -93,14 +133,23 @@ class TranslateService {
       professionText,
       luckText,
       personal_lifeText,
-      travelText
-    ]
+      travelText,
+    ];
     predictionObj.birth_moon_sign = birth_moon_sign;
     predictionObj.birth_moon_nakshatra = birth_moon_nakshatra;
     predictionObj.name = name;
     predictionObj.sunSign = sunsign;
     predictionObj.prediction = finalTranslatedPredicitonObj;
-    predictionObj.translatedTitles = translatedTitles
+    predictionObj.translatedTitles = translatedTitles;
+    let trandslatedPredictionDate = prediction_date
+    if(lang == 'hi') {
+      trandslatedPredictionDate = this.convertToHindiNumbers(prediction_date)
+    }
+    else if(lang == 'gu') {
+      trandslatedPredictionDate = this.convertToHindiNumbers(prediction_date)
+    }
+    predictionObj.prediction_date = trandslatedPredictionDate;
+
     return predictionObj;
   }
 }
